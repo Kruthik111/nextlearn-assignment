2f1f59703d4bcbfcc756ea1047ae45bfda1ad8a5
Kruthik S <gokruthik2003@gmail.com>
2025-03-01 12:01:12 +0530

    changes

--- Frontend/src/Pages/RegisterUser.jsx
+++ Frontend/src/Pages/RegisterUser.jsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material";
+import { Button, Paper, TextField } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
@@ -42,52 +42,54 @@ const RegisterUser = () => {
   };
 
   return (
-    <Box>
-      <h1>Create user</h1>
-      <Stack
-        sx={{
-          maxWidth: 300,
-          mb: 10,
-        }}
-        gap={2}
-      >
-        <form method="POST">
-          <TextField
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Name"
-          />
-          <TextField
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder="Email"
-          />
-          <TextField
-            type="number"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            placeholder="Phone"
-          />
-          <TextField
-            type="text"
-            name="city"
-            value={formData.city}
-            onChange={handleChange}
-            placeholder="City"
-            maxLength={10}
-            min={11111111}
-          />
-          <Button onClick={handleSubmit} variant="contained">
-            Add User
-          </Button>
-        </form>
-      </Stack>
-    </Box>
+    <Paper>
+      <Box>
+        <h1>Create user </h1>
+        <Stack
+          sx={{
+            maxWidth: 300,
+            mb: 10,
+          }}
+          gap={2}
+        >
+          <form method="POST">
+            <TextField
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              placeholder="Name"
+            />
+            <TextField
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+            />
+            <TextField
+              type="number"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              placeholder="Phone"
+            />
+            <TextField
+              type="text"
+              name="city"
+              value={formData.city}
+              onChange={handleChange}
+              placeholder="City"
+              maxLength={10}
+              min={11111111}
+            />
+            <Button onClick={handleSubmit} variant="contained">
+              Add User
+            </Button>
+          </form>
+        </Stack>
+      </Box>
+    </Paper>
   );
 };
 
